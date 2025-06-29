3d4ec01b0e746667ae0ab4f49060c243a954f725
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2025-06-29 11:27:48 +0000

    overflow

--- src/AlbumInfo.jsx
+++ src/AlbumInfo.jsx
@@ -2,8 +2,8 @@ const AlbumInfo=({item})=>{
  
 return (
 
-  <div className="flex flex-row mx-2 mt-4 justify-center items-center"><img src={item.album_pic} className="h-20 w-20 sm:h-28 sm:w-28 lg:h-32 lg:w-32 m-2"/>
+  <div className="flex flex-row mx-2 mt-4 justify-center items-center" style={{maxWidth:"80vw"}}><img src={item.album_pic} className="h-20 w-20 sm:h-28 sm:w-28 lg:h-32 lg:w-32 m-2"/>
 <div>
-<div className="text-wrap text-base sm:text-xl mx-2">{item.album_name}</div><div className="text-base sm:text-xl mx-2">{item.album_date}</div><div className="mx-2 text-xs">{item.genres}</div></div></div>)
+<div className="text-wrap text-base sm:text-xl mx-2">{item.album_name}</div><div className="text-base sm:text-xl mx-2">{item.album_date}</div><div className="mx-2 text-xs text-wrap">{item.genres}</div></div></div>)
 }
 export default AlbumInfo;
--- src/TrackList.jsx
+++ src/TrackList.jsx
@@ -2,10 +2,10 @@ import {Tooltip} from "react-tooltip";
 import AlbumInfo from "./AlbumInfo";
 import SongTitles from "./SongTitles";
 const TrackList = ({ tracks }) => {
-  return (<ul className="list-group grid grid-cols-2 mt-8">
+  return (<ul className="list-group grid grid-cols-2 mt-4">
       {tracks.map((track,index) => (
         <li key={track.track}><div className="ml-4 mb-4">
-<Tooltip id={`my-anchor-element-${index}`}><div><AlbumInfo item={track}/><SongTitles item={track}/></div></Tooltip><a data-tooltip-id={`my-anchor-element-${index}`}>
+<Tooltip id={`my-anchor-element-${index}`}><div style={{overflowY:"scroll",maxHeight:"60vh"}}><AlbumInfo item={track}/><SongTitles item={track}/></div></Tooltip><a data-tooltip-id={`my-anchor-element-${index}`}>
 <img src={track.album_pic} className="h-16 w-16 sm:h-32 sm:w-32" style={{border:"1px solid #0f172a"}}/></a>
 <div className="sm:text-xl md:text-2xl xl:text-3xl mt-2 text-white" style={{fontSize:"0.84rem"}}>{track.track}</div></div></li>
       ))}
